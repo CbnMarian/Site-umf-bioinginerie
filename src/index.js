4ab61205140ce6833eb4e1f92394ed38e1a155b5
CbnMarian <m.marian03@yahoo.com>
2024-03-19 19:59:17 +0200

    3

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Footer from "./components/footer/footer";
+import Team from "./components/team-page/team-presentation";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Team />
     <Footer />
   </React.StrictMode>
 );
