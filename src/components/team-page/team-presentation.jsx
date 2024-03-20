0b18c7ee21e73b829e388b75065ea31410ffd9a1
CbnMarian <m.marian03@yahoo.com>
2024-03-20 12:33:34 +0200

    added swiper

--- src/components/team-page/team-presentation.jsx
+++ src/components/team-page/team-presentation.jsx
@@ -1,70 +1,97 @@
 import "./team-presentation.css";
+import member1 from "../../assets/face.png";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const Team = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
-    <section>
-      <div className="team-container">
-        <h1>SCIENTIFIC TEAM</h1>
-        <div>
-          <div className="container with-coordinator">
-            <div className="top-square">
-              <img src="main_coordinator_photo.jpg" alt="Main Coordinator" />
-            </div>
-            <div className="container with-team">
-              <div className="team-grid">
-                <div className="team-member">
-                  <img
-                    src="main_coordinator_photo.jpg"
-                    alt="Team Member: Alice Smith"
-                  />
-                  <p>Team Member</p>
-                </div>
-                <div className="team-member">
-                  <img
-                    src="main_coordinator_photo.jpg"
-                    alt="Team Member: Alice Smith"
-                  />
-                  <p>Team Member</p>
-                </div>
-                <div className="team-member">
-                  <img
-                    src="main_coordinator_photo.jpg"
-                    alt="Team Member: Alice Smith"
-                  />
-                  <p>Team Member</p>
-                </div>
-                <div className="team-member">
-                  <img
-                    src="main_coordinator_photo.jpg"
-                    alt="Team Member: Alice Smith"
-                  />
-                  <p>Team Member</p>
-                </div>
-                <div className="team-member">
-                  <img
-                    src="main_coordinator_photo.jpg"
-                    alt="TTeam Member: Alice Smith"
-                  />
-                  <p>Team Member</p>
-                </div>
-                <div className="team-member">
-                  <img
-                    src="main_coordinator_photo.jpg"
-                    alt="Team Member: Alice Smith"
-                  />
-                  <p>Team Member</p>
-                </div>
-                <div className="team-member">
-                  <img
-                    src="main_coordinator_photo.jpg"
-                    alt="Team Member: Alice Smith"
-                  />
-                  <p>Team Member</p>
-                </div>
-              </div>
-            </div>
+    <section id="container">
+      <div id="main">
+        <div className="team-leader">
+          <h1 className="title">SCIENTIFIC TEAM</h1>
+          <img src={member1} alt="Team Member: Alice Smith" />
+          <h3>Team Member1</h3>
+          <div>
+            <p>Description</p>
           </div>
         </div>
+
+        <Carousel responsive={responsive}>
+          <div className="team-member">
+            <img src={member1} alt="Team Member: Alice Smith" />
+            <h3>Team Member1</h3>
+            <div>
+              <p>Description</p>
+            </div>
+          </div>
+
+          <div className="team-member">
+            <img src={member1} alt="Team Member: Alice Smith" />
+            <h3>Team Member2</h3>
+            <div>
+              <p>Description</p>
+            </div>
+          </div>
+
+          <div className="team-member">
+            <img src={member1} alt="Team Member: Alice Smith" />
+            <h3>Team Member3</h3>
+            <div>
+              <p>Description</p>
+            </div>
+          </div>
+
+          <div className="team-member">
+            <img src={member1} alt="Team Member: Alice Smith" />
+            <h3>Team Member4</h3>
+            <div>
+              <p>Description</p>
+            </div>
+          </div>
+
+          <div className="team-member">
+            <img src={member1} alt="Team Member: Alice Smith" />
+            <h3>Team Member5</h3>
+            <div>
+              <p>Description</p>
+            </div>
+          </div>
+
+          <div className="team-member">
+            <img src={member1} alt="Team Member: Alice Smith" />
+            <h3>Team Member6</h3>
+            <div>
+              <p>Description</p>
+            </div>
+          </div>
+
+          <div className="team-member">
+            <img src={member1} alt="Team Member: Alice Smith" />
+            <h3>Team Member7</h3>
+            <div>
+              <p>Description</p>
+            </div>
+          </div>
+        </Carousel>
       </div>
     </section>
   );
