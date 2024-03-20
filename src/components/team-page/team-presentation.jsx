b12fe6a1d7fc5ac1fc40a2bf2119bde90f6808f8
CbnMarian <m.marian03@yahoo.com>
2024-03-20 16:08:12 +0200

    team comp 80%

--- src/components/team-page/team-presentation.jsx
+++ src/components/team-page/team-presentation.jsx
@@ -1,97 +1,76 @@
 import "./team-presentation.css";
-import member1 from "../../assets/face.png";
-import Carousel from "react-multi-carousel";
+import member1 from "../../assets/Verestiuc.png";
+import member2 from "../../assets/blank.png";
 import "react-multi-carousel/lib/styles.css";
 
 const Team = () => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
   return (
     <section id="container">
       <div id="main">
         <div className="team-leader">
           <h1 className="title">SCIENTIFIC TEAM</h1>
-          <img src={member1} alt="Team Member: Alice Smith" />
-          <h3>Team Member1</h3>
-          <div>
-            <p>Description</p>
+          <div className="team-member">
+            <img src={member1} alt="Team Member: Liliana Verestiuc" />
+            <h3>
+              <span className="title-prefix">Prof. dr.</span> Liliana Verestiuc
+            </h3>
+            <p>
+              <a href="mailto:liliana.verestiuc@umfiasi.ro">
+                liliana.verestiuc@umfiasi.ro
+              </a>
+            </p>
           </div>
         </div>
 
-        <Carousel responsive={responsive}>
+        <div className="members member">
           <div className="team-member">
-            <img src={member1} alt="Team Member: Alice Smith" />
-            <h3>Team Member1</h3>
+            <img src={member2} alt="Team Member: Alice Smith" />
+            <h3>Vera Balan</h3>
             <div>
               <p>Description</p>
             </div>
           </div>
 
           <div className="team-member">
-            <img src={member1} alt="Team Member: Alice Smith" />
-            <h3>Team Member2</h3>
+            <img src={member2} alt="Team Member: Alice Smith" />
+            <h3>Maria Butnaru</h3>
             <div>
               <p>Description</p>
             </div>
           </div>
 
           <div className="team-member">
-            <img src={member1} alt="Team Member: Alice Smith" />
-            <h3>Team Member3</h3>
+            <img src={member2} alt="Team Member: Alice Smith" />
+            <h3>Isabella Cobzariu</h3>
             <div>
               <p>Description</p>
             </div>
           </div>
 
           <div className="team-member">
-            <img src={member1} alt="Team Member: Alice Smith" />
-            <h3>Team Member4</h3>
+            <img src={member2} alt="Team Member: Alice Smith" />
+            <h3>Florina Daniela Ivan</h3>
             <div>
               <p>Description</p>
             </div>
           </div>
 
           <div className="team-member">
-            <img src={member1} alt="Team Member: Alice Smith" />
-            <h3>Team Member5</h3>
+            <img src={member2} alt="Team Member: Alice Smith" />
+            <h3>Andreea Luca</h3>
             <div>
               <p>Description</p>
             </div>
           </div>
 
           <div className="team-member">
-            <img src={member1} alt="Team Member: Alice Smith" />
-            <h3>Team Member6</h3>
+            <img src={member2} alt="Team Member: Alice Smith" />
+            <h3>Vlad Ursachi</h3>
             <div>
               <p>Description</p>
             </div>
           </div>
-
-          <div className="team-member">
-            <img src={member1} alt="Team Member: Alice Smith" />
-            <h3>Team Member7</h3>
-            <div>
-              <p>Description</p>
-            </div>
-          </div>
-        </Carousel>
+        </div>
       </div>
     </section>
   );
