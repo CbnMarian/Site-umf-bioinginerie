a08fd233cf8dba130bbbde697074364363b72463
CbnMarian <m.marian03@yahoo.com>
2024-03-27 14:37:47 +0200

    translated db

--- src/components/projects/data-projects.js
+++ src/components/projects/data-projects.js
@@ -1,143 +1,144 @@
 const DataProjects = {
-  "Proiecte cu finanțare națională - coordonator": [
+  "Projects with national funding - coordinator": [
     {
       post_id: 1,
       post_project_code: "PNCDI-II 132/2014",
       post_title:
-        "Suporturi magnetice biomimetice ca strategie alternativa pentru ingineria și repararea tesutului osos – MAGBIOTISS.",
+        "Biomimetic Magnetic Supports as an Alternative Strategy for Bone Tissue Engineering and Repair – MAGBIOTISS.",
       post_description:
-        "Director de proiect: prof. univ. dr. Liliana Vereștiuc, perioada: 2014 – 2017, buget: 450000 lei.",
+        "Project Director: Prof. Univ. Dr. Liliana Vereștiuc, period: 2014 – 2017, budget: 450,000 lei.",
     },
     {
       post_id: 2,
       post_project_code: "PNI-II-RU-PD-2012-3-0282",
       post_title:
-        "Particule magnetice funcţionalizate cu potenţiale aplicaţii ca mijloc teranostic.",
+        "Functionalized Magnetic Particles with Potential Applications as Theranostic Agents.",
       post_description:
-        "Director de proiect: asist. univ. dr. Vera Balan, buget: 300000 RON, perioada: 2012 – 2015.",
+        "Project Director: Assist. Prof. Dr. Vera Balan, budget: 300,000 RON, period: 2012 – 2015.",
     },
     {
       post_id: 3,
       post_project_code: "PN-III-P1-1.1-PD-2019-0937",
       post_title:
-        "Biodegradable and bioactive 3D composite scaffolds for bone regeneration in osteoporosis (SCAREGOST) / Suporturi compozite tridimensionale, biodegradabile și bioactive pentru regenerarea osoasă în osteoporoză.",
+        "Biodegradable and Bioactive 3D Composite Scaffolds for Bone Regeneration in Osteoporosis (SCAREGOST).",
       post_description:
-        "Director de proiect: asist. cerc. dr. Cojocaru Florina Daniela, perioada: 01.04.2021-31.03.2023, buget: 246950 lei",
+        "Project Director: Research Assistant Dr. Cojocaru Florina Daniela, period: 01.04.2021-31.03.2023, budget: 246,950 lei",
     },
     {
       post_id: 4,
       post_project_code: "PN-III-P2-2.1-PED-2019-4524",
       post_title:
-        "3D Advanced smart magnetic scaffolds for bone tissue engineering and regeneration.",
+        "3D Advanced Smart Magnetic Scaffolds for Bone Tissue Engineering and Regeneration.",
       post_description:
-        "Director de proiect: prof. univ. dr. ing. Vereștiuc Liliana, perioada: 23.10.2020-22.10.2022, buget: 250000 lei",
+        "Project Director: Prof. Univ. Dr. Eng. Liliana Vereștiuc, period: 23.10.2020-22.10.2022, budget: 250,000 lei",
     },
     {
       post_id: 5,
       post_project_code: "PN-III-P1-1.1-TE-2019-1671",
       post_title:
-        "Nanostructuri magnetice co-incapsulate pentru aplicatii de tip teranostic in cancerul mamar: studii in vivo si in vitro.",
+        "Co-encapsulated Magnetic Nanostructures for Theranostic Applications in Breast Cancer: In Vivo and In Vitro Studies.",
       post_description:
-        "Director de proiect: șef lucr. dr. Balan Vera, perioada: 15.09.2020-13.09.2022, buget: 431900 lei",
+        "Project Director: Assoc. Prof. Dr. Vera Balan, period: 15.09.2020-13.09.2022, budget: 431,900 lei",
     },
   ],
   "Proiecte cu finanțare națională - partener": [
     {
       post_id: 1,
       post_project_code: "PN-III-P1-1.2-PCCDI2017-0407/2018",
-      post_title: "Materiale inteligente pentru aplicatii medicale.",
+      post_title: "Smart Materials for Medical Applications.",
       post_description:
-        "Coordonator: Universitatea Politehnică București, buget proiect: 5287500 lei, perioada: 2018 – 2020, responsabil partener UMF Iași: prof. univ. dr. Liliana Vereștiuc, buget UMF Iași: 92400 lei.",
+        "Coordinator: Polytechnic University of Bucharest, project budget: 5,287,500 lei, period: 2018 – 2020, responsible partner UMF Iași: Prof. Univ. Dr. Liliana Vereștiuc, UMF Iași budget: 92,400 lei.",
     },
     {
       post_id: 2,
       post_project_code: "PN-III-P1-1.2-PCCDI2017-0239/2018",
       post_title:
-        "Obținerea și expertizarea unor noi materiale biocompatibile pentru aplicații medicale.",
+        "Obtaining and Expertise of New Biocompatible Materials for Medical Applications.",
       post_description:
-        "Coordonator: Universitatea Tehnică Gh. Asachi, Iași. Responsabil partener UMF Iași: prof. univ. dr. Norina Forna, perioada: 2018 – 2020, buget proiect: 5273400lei",
+        "Coordinator: Gheorghe Asachi Technical University, Iași. Responsible partner UMF Iași: Prof. Univ. Dr. Norina Forna, period: 2018 – 2020, project budget: 5,273,400 lei",
     },
     {
       post_id: 3,
       post_project_code: "PNCDI-II 122/03.07.2012",
       post_title:
-        "Cercetare interdisciplinară privind particule hibride multifuncționale pentru bio-cerințe - INTERBIORES.",
+        "Interdisciplinary Research on Multifunctional Hybrid Particles for Bio-requirements - INTERBIORES.",
       post_description:
-        "Coordonator: Institutul de Chimie Macromoleculară P. Poni Iași, buget proiect: 3323334 lei. Responsabil partener UMF Iași: prof. univ. dr. Liliana Vereștiuc, perioada: 2012 – 2016, buget UMF Iași: 700000 lei.",
+        "Coordinator: Petru Poni Institute of Macromolecular Chemistry, Iași, project budget: 3,323,334 lei. Responsible partner UMF Iași: Prof. Univ. Dr. Liliana Vereștiuc, period: 2012 – 2016, UMF Iași budget: 700,000 lei.",
     },
     {
       post_id: 4,
       post_project_code: "PNCDI-II, nr. 81051/3/12.09.2007",
       post_title:
-        "Cercetări privind implementarea separării magnetice în detoxifierea sângelui uman utilizând particule magnetice suport.",
+        "Research on the Implementation of Magnetic Separation in Human Blood Detoxification using Support Magnetic Particles.",
       post_description:
-        "Coordonator: Institutul de Fizică Tehnică, Iași, buget proiect: 2000000 lei. Responsabil partener UMF Iași: prof. univ. dr. Liliana Vereștiuc, perioada: 2007 – 2010, buget UMF Iași: 445000 lei.",
+        "Coordinator: Institute of Technical Physics, Iași, project budget: 2,000,000 lei. Responsible partner UMF Iași: Prof. Univ. Dr. Liliana Vereștiuc, period: 2007 – 2010, UMF Iași budget: 445,000 lei.",
     },
     {
       post_id: 5,
       post_project_code: "Contract CEEX nr. 10/2005",
       post_title:
-        "Arhitecturi inovative degradabile, biocompatibile și bioactive pe bază de polimeri naturali şi sintetici.",
+        "Innovative Degradable, Biocompatible and Bioactive Architectures based on Natural and Synthetic Polymers.",
       post_description:
-        "Coordonator: Institutul de Chimie Macromoleculară P. Poni Iași, buget proiect: 1200000 lei. Responsabil partener UMF Iași: prof. univ. dr. Liliana Vereștiuc, perioada: 2005 – 2008, buget UMF Iași: 180000 lei.",
+        "Coordinator: Petru Poni Institute of Macromolecular Chemistry, Iași, project budget: 1,200,000 lei. Responsible partner UMF Iași: Prof. Univ. Dr. Liliana Vereștiuc, period: 2005 – 2008, UMF Iași budget: 180,000 lei.",
     },
     {
       post_id: 6,
       post_project_code: "Contract CERES nr.4-113/2004",
-      post_title: "Cercetări aplicative în domeniul biostructurilor magnetice.",
+      post_title: "Applied Research in the Field of Magnetic Biostructures.",
       post_description:
-        "Coordonator: Institutul de Chimie Macromoleculară P. Poni Iași, buget proiect: 100000 lei. Responsabil partener UMF Iași: prof. univ. dr. Liliana Vereștiuc, perioada: 2004-2006, buget UMF Iași: 15000 lei.",
+        "Coordinator: Petru Poni Institute of Macromolecular Chemistry, Iași, project budget: 100,000 lei. Responsible partner UMF Iași: Prof. Univ. Dr. Liliana Vereștiuc, period: 2004-2006, UMF Iași budget: 15,000 lei.",
     },
     {
       post_id: 7,
       post_project_code: "Contract CERES nr.4-514/2004",
       post_title:
-        "Structuri carbonice funcționale cu aplicații în implantologia cardio-vasculară.",
+        "Functional Carbon Structures with Applications in Cardiovascular Implantology.",
       post_description:
-        "Coordonator: Institutul de Chimie Macromoleculară P. Poni Iași, buget proiect: 100000 lei. Responsabil partener UMF Iași: prof. univ. dr. Liliana Vereștiuc, perioada: 2004-2006, buget UMF Iași: 18000 lei.",
+        "Coordinator: Petru Poni Institute of Macromolecular Chemistry, Iași, project budget: 100,000 lei. Responsible partner UMF Iași: Prof. Univ. Dr. Liliana Vereștiuc, period: 2004-2006, UMF Iași budget: 18,000 lei.",
     },
     {
       post_id: 8,
       post_project_code: "Contract CERES nr.3-49/2003",
       post_title:
-        "Sisteme polimere purtătoare de principii biologic active (medicamente și enzime).",
+        "Polymeric Systems Carrying Biologically Active Principles (Medicines and Enzymes).",
       post_description:
-        "Coordonator: Universitatea Tehnică Gh. Asachi, Iași, buget proiect: 100000 lei. Responsabil partener UMF Iași: prof. univ. dr. Liliana Vereștiuc, perioada: 2003-2005, buget UMF Iași: 27250 lei.",
+        "Coordinator: Gheorghe Asachi Technical University, Iași, project budget: 100,000 lei. Responsible partner UMF Iași: Prof. Univ. Dr. Liliana Vereștiuc, period: 2003-2005, UMF Iași budget: 27,250 lei.",
     },
     {
       post_id: 9,
       post_project_code: "Contract CERES nr.3-51/2003",
       post_title:
-        "Materiale polimere multifazice-biodegradabile, biocompatibile, biologic active, sisteme motile artificiale – cu aplicații în medicină și pentru asigurarea protecției mediului.",
+        "Multifaceted Biodegradable, Biocompatible, Biologically Active Polymer Materials, Artificial Motile Systems – with Applications in Medicine and Environmental Protection.",
       post_description:
-        'Coordonator: Universitatea Tehnică "Gh. Asachi, Iași", buget proiect: 189400 lei. Responsabil partener UMF Iași: prof. univ. dr. Liliana Vereștiuc, perioada: 2004-2005, buget UMF Iași: 13250 lei.',
+        "Coordinator: Gheorghe Asachi Technical University, Iași, project budget: 189,400 lei. Responsible partner UMF Iași: Prof. Univ. Dr. Liliana Vereștiuc, period: 2004-2005, UMF Iași budget: 13,250 lei.",
     },
     {
       post_id: 10,
       post_project_code: "Contract BIOTECH nr. 105/19.10.2001",
       post_title:
-        "Hidrogeluri inteligente pe bază de polimeri naturali și sintetici cu aplicații în medicină, farmacie, biotehnologie.",
+        "Intelligent Hydrogels based on Natural and Synthetic Polymers with Applications in Medicine, Pharmacy, Biotechnology.",
       post_description:
-        "Coordonator: Institutul Național de Cercetare-Dezvoltare pentru Științe Biologice București, buget proiect: 150000 lei. Responsabil partener UMF Iași: prof. univ. dr. Liliana Vereștiuc, perioada: 2001-2004, buget UMF Iași: 40900 lei.",
+        "Coordinator: National Institute of Research and Development for Biological Sciences Bucharest, project budget: 150,000 lei. Responsible partner UMF Iași: Prof. Univ. Dr. Liliana Vereștiuc, period: 2001-2004, UMF Iași budget: 40,900 lei.",
     },
   ],
-  "Granturi interne": [
+
+  "Projects with national funding - partner": [
     {
       post_id: 1,
-      post_project_code: "Grant Intern UMF Iași 17075/07.10.2009",
+      post_project_code: "Internal Grant UMF Iași 17075/07.10.2009",
       post_title:
-        "Studiul hemocompatibilității unor derivați poliuretanici cu potențiale aplicații biomedicale.",
+        "Study of the Hemocompatibility of Some Polyurethane Derivatives with Potential Biomedical Applications.",
       post_description:
-        "Director de proiect: conf. univ. dr. Maria Butnaru, perioada: 2009– 2010, buget: 19100 lei.",
+        "Project Director: Assoc. Prof. Dr. Maria Butnaru, period: 2009–2010, budget: 19,100 lei.",
     },
     {
       post_id: 2,
       post_project_code:
-        'GRANTUL Tânărului Cercetător UMF „Grigore T. Popa" Iași',
+        'Grant for Young Researchers UMF "Grigore T. Popa" Iași',
       post_title:
-        "Development and in vitro evaluation of magnetic nanosystems as multifunctional entities for breast cancer therapy.",
+        "Development and In Vitro Evaluation of Magnetic Nanosystems as Multifunctional Entities for Breast Cancer Therapy.",
       post_description:
-        "Director de proiect: șef lucr. dr. Balan Vera, perioada: 01.01.2019-31.12.2020, buget: 22900 lei​",
+        "Project Director: Assoc. Prof. Dr. Vera Balan, period: 01.01.2019-31.12.2020, budget: 22,900 lei",
     },
   ],
 };
